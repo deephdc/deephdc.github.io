9a31326e031c967f0e26265ac9a66bc7311f3f2f
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2020-01-20 18:09:27 +0100

    Update entry design

--- pelicanconf.py
+++ pelicanconf.py
@@ -68,7 +68,7 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
-DEFAULT_PAGINATION = 6
+DEFAULT_PAGINATION = 30
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
